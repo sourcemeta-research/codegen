f5cafe5586b1f28b3f97d99cbd70910aaaea7b82
Juan Cruz Viotti <jv@jviotti.com>
2025-12-23 15:28:39 -0400

    Simpler type generation
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- src/generator/typescript.cc
+++ src/generator/typescript.cc
@@ -15,10 +15,10 @@ static auto scalar_type_to_typescript(IRScalarType type) -> std::string {
 
 static auto handle_ir_scalar(std::ostream &output, const IRScalar &entry,
                              const std::string &default_namespace) -> void {
-  const auto name{to_pascal_case(entry.pointer, entry.instance_location,
-                                 default_namespace)};
-  output << "export type " << name << " = "
-         << scalar_type_to_typescript(entry.value) << ";\n";
+  output << "export type "
+         << to_pascal_case(entry.pointer, entry.instance_location,
+                           default_namespace)
+         << " = " << scalar_type_to_typescript(entry.value) << ";\n";
 }
 
 static auto handle_ir_union(std::ostream &, const IRUnion &,
@@ -28,17 +28,17 @@ static auto handle_ir_union(std::ostream &, const IRUnion &,
 
 static auto handle_ir_object(std::ostream &output, const IRObject &entry,
                              const std::string &default_namespace) -> void {
-  const auto name{to_pascal_case(entry.pointer, entry.instance_location,
-                                 default_namespace)};
-  output << "export interface " << name << " {\n";
+  output << "export interface "
+         << to_pascal_case(entry.pointer, entry.instance_location,
+                           default_namespace)
+         << " {\n";
   for (const auto &[member_name, member_value] : entry.members) {
     const auto optional_marker{member_value.required ? "" : "?"};
     const auto readonly_marker{member_value.immutable ? "readonly " : ""};
-    const auto member_type_name{to_pascal_case(member_value.pointer,
-                                               member_value.instance_location,
-                                               default_namespace)};
     output << "  " << readonly_marker << member_name << optional_marker << ": "
-           << member_type_name << ";\n";
+           << to_pascal_case(member_value.pointer,
+                             member_value.instance_location, default_namespace)
+           << ";\n";
   }
   output << "}\n";
 }
