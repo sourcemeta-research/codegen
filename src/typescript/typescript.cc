2bbc206d686ef35eba75f0575492235ed03a8f5f
Juan Cruz Viotti <jv@jviotti.com>
2025-12-23 14:59:19 -0400

    Typescript cleanup
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- src/typescript/typescript.cc
+++ src/typescript/typescript.cc
@@ -1,7 +1,6 @@
 #include <sourcemeta/codegen/typescript.h>
 
 #include <stdexcept> // std::runtime_error
-#include <variant>   // std::visit
 
 namespace sourcemeta::codegen {
 
@@ -37,6 +36,40 @@ static auto scalar_type_to_typescript(IRScalarType type) -> std::string {
   }
 }
 
+static auto
+handle_ir_scalar(std::ostream &output, const IRScalar &entry,
+                 const std::optional<std::string> &default_namespace) -> void {
+  const auto name{pointer_to_name(entry.instance_location, default_namespace)};
+  output << "export type " << name << " = "
+         << scalar_type_to_typescript(entry.value) << ";\n";
+}
+
+static auto handle_ir_union(std::ostream &, const IRUnion &,
+                            const std::optional<std::string> &) -> void {
+  throw std::runtime_error("IRUnion is not supported yet");
+}
+
+static auto
+handle_ir_object(std::ostream &output, const IRObject &entry,
+                 const std::optional<std::string> &default_namespace) -> void {
+  const auto name{pointer_to_name(entry.instance_location, default_namespace)};
+  output << "export interface " << name << " {\n";
+  for (const auto &[member_name, member_value] : entry.members) {
+    const auto optional_marker{member_value.required ? "" : "?"};
+    const auto readonly_marker{member_value.immutable ? "readonly " : ""};
+    const auto member_type_name{
+        pointer_to_name(member_value.instance_location, default_namespace)};
+    output << "  " << readonly_marker << member_name << optional_marker << ": "
+           << member_type_name << ";\n";
+  }
+  output << "}\n";
+}
+
+static auto handle_ir_impossible(std::ostream &, const IRImpossible &,
+                                 const std::optional<std::string> &) -> void {
+  throw std::runtime_error("IRImpossible is not supported yet");
+}
+
 auto typescript(std::ostream &output, const IRResult &result,
                 const std::optional<std::string> &default_namespace) -> void {
   bool first{true};
@@ -45,30 +78,16 @@ auto typescript(std::ostream &output, const IRResult &result,
       output << "\n";
     }
     first = false;
-    std::visit(
-        [&output, &default_namespace](const auto &entry) {
-          using T = std::decay_t<decltype(entry)>;
-          const auto name{
-              pointer_to_name(entry.instance_location, default_namespace)};
 
-          if constexpr (std::is_same_v<T, IRScalar>) {
-            output << "export type " << name << " = "
-                   << scalar_type_to_typescript(entry.value) << ";\n";
-          } else if constexpr (std::is_same_v<T, IRObject>) {
-            output << "export interface " << name << " {\n";
-            for (const auto &[member_name, member_value] : entry.members) {
-              const auto optional_marker{member_value.required ? "" : "?"};
-              const auto readonly_marker{member_value.immutable ? "readonly "
-                                                                : ""};
-              const auto member_type_name{pointer_to_name(
-                  member_value.instance_location, default_namespace)};
-              output << "  " << readonly_marker << member_name
-                     << optional_marker << ": " << member_type_name << ";\n";
-            }
-            output << "}\n";
-          }
-        },
-        entity);
+    if (const auto *scalar = std::get_if<IRScalar>(&entity)) {
+      handle_ir_scalar(output, *scalar, default_namespace);
+    } else if (const auto *union_entry = std::get_if<IRUnion>(&entity)) {
+      handle_ir_union(output, *union_entry, default_namespace);
+    } else if (const auto *object = std::get_if<IRObject>(&entity)) {
+      handle_ir_object(output, *object, default_namespace);
+    } else if (const auto *impossible = std::get_if<IRImpossible>(&entity)) {
+      handle_ir_impossible(output, *impossible, default_namespace);
+    }
   }
 }
 
