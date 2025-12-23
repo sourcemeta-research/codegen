ae856f4faa9ab3aa23d8912e6cfc6181f4cd11d7
Juan Cruz Viotti <jv@jviotti.com>
2025-12-23 15:14:16 -0400

    Make the contrib tool work
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- contrib/typescript.cc
+++ contrib/typescript.cc
@@ -1,6 +1,40 @@
 #include <sourcemeta/codegen/generator.h>
 #include <sourcemeta/codegen/ir.h>
 
-#include <cstdlib> // EXIT_SUCCESS
+#include <sourcemeta/core/json.h>
+#include <sourcemeta/core/jsonschema.h>
+#include <sourcemeta/core/options.h>
 
-auto main(int, char **) noexcept -> int { return EXIT_SUCCESS; }
+#include <cstdlib>    // EXIT_SUCCESS, EXIT_FAILURE
+#include <filesystem> // std::filesystem::path
+#include <iostream>   // std::cout, std::cerr
+#include <optional>   // std::optional, std::nullopt
+#include <string>     // std::string
+
+auto main(int argc, char *argv[]) -> int {
+  sourcemeta::core::Options options;
+  options.option("default-namespace", {"n"});
+  options.parse(argc, argv);
+
+  const auto &positional_arguments{options.positional()};
+  if (positional_arguments.empty()) {
+    std::cerr << "error: missing schema path\n";
+    return EXIT_FAILURE;
+  }
+
+  const std::filesystem::path schema_path{positional_arguments.front()};
+  const auto schema{sourcemeta::core::read_json(schema_path)};
+
+  const auto result{
+      sourcemeta::codegen::compile(schema, sourcemeta::core::schema_walker,
+                                   sourcemeta::core::schema_resolver,
+                                   sourcemeta::codegen::default_compiler)};
+
+  std::optional<std::string> default_namespace{std::nullopt};
+  if (options.contains("default-namespace")) {
+    default_namespace = std::string{options.at("default-namespace").front()};
+  }
+
+  sourcemeta::codegen::typescript(std::cout, result, default_namespace);
+  return EXIT_SUCCESS;
+}
